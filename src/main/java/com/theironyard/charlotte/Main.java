ae3907fff01ea941b247335c5cc48e7a04e029ae
Emileen Marianayagam <emileenmarianayagam@Emileens-MacBook-Pro.local>
2016-12-20 14:50:44 -0500

    update

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -6,7 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        String port = System.getenv("PORT");
+
+        if (port != null) {
+            Spark.port(Integer.valueOf(port));
+        }
+
+
         Spark.get("/", (request, response) -> "Hello World!");
+
+        if(port !=null){
+
+        }
 
         Spark.before(((request, response) -> {response.header("Access-Control-Allow-Origin", "*");}));
 
